ca4141db8eb591fdb3df45299359220f0e368ec0
valeriyl <valeriy.leshchuk@gmail.com>
2019-04-18 21:22:15 +0300

    Logger - adjust time format, add support for boolean types

--- src/hw/utils/Logger.java
+++ src/hw/utils/Logger.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public abstract class Logger
 {
     private static Date date_ = new Date();
-    private static final String DEFAULT_DATE_TIME_FORMAT = "[dd-MMM-yyyy HH:mm:ss a]";
+    private static final String DEFAULT_DATE_TIME_FORMAT = "[dd-MMM-yyyy HH:mm:ss:sss a]";
     private static final SimpleDateFormat dateFormat_ = new SimpleDateFormat(DEFAULT_DATE_TIME_FORMAT);
 
     public static void info(String str)
@@ -14,9 +14,19 @@ public abstract class Logger
         System.out.println(currentTime() + " info: " + str);
     }
 
+    public static void info(Boolean expr)
+    {
+        info(expr ? "true" : "false");
+    }
+
     public static void warning(String str)
     {
         System.out.println(currentTime() + " info: " + str);
+    }
+
+    public static void warning(Boolean expr)
+    {
+        warning(expr ? "true" : "false");
     }
 
     private static String currentTime()
