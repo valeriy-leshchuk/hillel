3246f42db1e6075e6ed04af23c76c7361c1f4570
valeriyl <valeriy.leshchuk@gmail.com>
2019-06-16 16:58:59 +0300

    HomeWork#20 - reworked

--- src/main/java/hw/twenty/App.java
+++ src/main/java/hw/twenty/App.java
@@ -8,15 +8,11 @@ public class App
     public static void main(String[] args)
     {
         IHashStorage<Integer, String> storage = new HashStorage<>();
-        storage.add(1, "one");
-        storage.add(2, "two");
-        storage.add(3, "three");
-        storage.add(4, "four");
-        storage.add(4, "four four");
 
-        log.debug(storage.get(1));
-        log.debug(storage.get(5));
-        log.debug(storage.get(3));
-        log.debug(storage.get(4));
+        storage.add(1,"one");
+        storage.add(5,"five");
+        log.debug(storage);
+        storage.add(9,"nine");
+        log.debug(storage);
     }
 }
--- src/main/java/hw/twenty/HashStorage.java
+++ src/main/java/hw/twenty/HashStorage.java
@@ -7,16 +7,43 @@ import lombok.extern.log4j.Log4j;
 
 import java.util.*;
 
+/**
+ * @param <K> key that will be stored
+ * @param <V> value that will be stored
+ */
 @Log4j
 public class HashStorage<K, V> implements IHashStorage<K, V>
 {
-    private static final int INIT_CAPACITY = 16;
+    /**
+     * Number of buckets that will be created upon storage creating.
+     */
+    private static final int INIT_CAPACITY = 4;
+
+    /**
+     * Coefficient that is used for resizing (always increasing the size) of storage.
+     * Storage will be resized and rebalanced when it's load is more than that.
+     */
     private static final double loadFactor = 0.75;
 
-    private int currentCapacity;
-    private int currentlyAddedEntities;
+    /**
+     * Stores the number of entities which is the threshold for resizing the storage.
+     * If after adding entity amount of existing entities is greater than this number - storage will get resized and rebalanced.
+     */
     private int resizeWhenSize;
 
+    /**
+     * Number of currently existing buckets in storage.
+     */
+    private int currentCapacity;
+
+    /**
+     * Number of added K,V pairs
+     */
+    private int currentlyAddedEntities;
+
+    /**
+     * Actual array for storing buckets.
+     */
     private ArrayList<Bucket> buckets = new ArrayList<>(INIT_CAPACITY);
 
     public HashStorage()
@@ -26,74 +53,118 @@ public class HashStorage<K, V> implements IHashStorage<K, V>
         fillNullBuckets();
     }
 
+    /**
+     * Once storage is created or resized, some of buckets don't exist. This method initialize them empty.
+     */
     private void fillNullBuckets()
     {
-        for (int i = 0; i < currentCapacity; i++)
+        int amountOfBucketsToAdd = currentCapacity - buckets.size();
+        for (int i = 0; i < amountOfBucketsToAdd; i++)
         {
-            try
-            {
-                buckets.get(i);
-            }
-            catch (IndexOutOfBoundsException ex)
-            {
-                buckets.add(new Bucket());
-            }
+            buckets.add(new Bucket());
         }
     }
 
+    /**
+     * @param hash is a hashCode of the key (<K>)
+     * @return index of bucket where given key should be added of searched for.
+     */
     private int defineBucket(int hash)
     {
         return hash % currentCapacity;
     }
 
+    /**
+     * This method performs resizing storage and rebalancing entities among buckets.
+     */
+    private void resizeAndRebalanceStorage()
+    {
+        ArrayList<Bucket> tmp = new ArrayList<>(buckets);
+        buckets = new ArrayList<>(currentCapacity);
+        currentlyAddedEntities = 0;
+        fillNullBuckets();
+        tmp.forEach(bucket -> bucket.getEntities().forEach(entity -> add(entity.getKey(), entity.getValue())));
+    }
+
+    /**
+     * Public method for adding a pair of key and value (K,V) to storage.
+     * @param key   key that is going to be added.
+     * @param value value that is going to be added.
+     * @return true if pair was added, false otherwise. In current implementation key can't be null.
+     */
     @Override
     public boolean add(K key, V value)
     {
-
-        int hashValueOfKey;
-        try
-        {
-            hashValueOfKey = key.hashCode();
-        }
-        catch (NullPointerException ex)
+        if (key == null)
         {
             return false;
         }
+        int hashValueOfKey = key.hashCode();
         int shouldBePlacedToBucket = defineBucket(hashValueOfKey);
 
         buckets.get(shouldBePlacedToBucket).add(key, value);
         currentlyAddedEntities++;
 
-        if (currentlyAddedEntities>=resizeWhenSize)
+        if (currentlyAddedEntities >= resizeWhenSize)
         {
-            currentCapacity *=2;
-            resizeWhenSize = (int)(currentCapacity*loadFactor);
-            fillNullBuckets();
+            currentCapacity = currentCapacity * 2;
+            resizeWhenSize = (int) (currentCapacity * loadFactor);
+            resizeAndRebalanceStorage();
         }
         return true;
     }
 
+    /**
+     * @param key is key for which the value should be returned.
+     * @return found value or null is the given key is null.
+     */
     @Override
     public V get(K key)
     {
-        int hashValueOfKey;
-        try
-        {
-            hashValueOfKey = key.hashCode();
-        }
-        catch (NullPointerException ex)
+        if (key == null)
         {
             return null;
         }
+        int hashValueOfKey = key.hashCode();
         int shouldBeSearchedInBucket = defineBucket(hashValueOfKey);
         return buckets.get(shouldBeSearchedInBucket).getValueByKey(key);
     }
 
+    @Override
+    public String toString()
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("HashStorage:");
+        buckets.forEach(bucket ->
+        {
+            sb.append("\nBucket: ");
+            bucket.getEntities().forEach(entity -> sb.append(entity));
+        });
+        return sb.toString();
+    }
+
+    /**
+     * Private class which represents a node of storage.
+     */
     private class Bucket
     {
+        /**
+         * List of pairs key,value (K,V) that are added bucket.
+         */
+        @Getter
         private List<Entity> entities = new LinkedList<>();
+
+        /**
+         * Keeps hashes of keys that are added to bucket.
+         */
         private List<Integer> hashesOfEntities = new LinkedList<>();
 
+        /**
+         * Method for adding pair of key and value (K,V) to bucket.
+         *
+         * @param key   is key of pair that is intended to be added.
+         * @param value is value of pair that is intended to be added.
+         */
         public void add(K key, V value)
         {
             int hashOfKey = key.hashCode();
@@ -106,7 +177,7 @@ public class HashStorage<K, V> implements IHashStorage<K, V>
             else
             {
                 Entity existingEntityWithKey = findEntityByKey(key);
-                if (existingEntityWithKey==null)
+                if (existingEntityWithKey == null)
                 {
                     entities.add(new Entity(key, value));
                 }
@@ -117,6 +188,12 @@ public class HashStorage<K, V> implements IHashStorage<K, V>
             }
         }
 
+        /**
+         * Method for getting entire entity from bucket by given key.
+         *
+         * @param key is the key of entity that is searched for.
+         * @return found entity or null is one is not found.
+         */
         private Entity findEntityByKey(K key)
         {
             for (Entity e : entities)
@@ -129,6 +206,10 @@ public class HashStorage<K, V> implements IHashStorage<K, V>
             return null;
         }
 
+        /**
+         * @param key is the key for which the value should be found.
+         * @return the value if found, null otherwise.
+         */
         public V getValueByKey(K key)
         {
             if (!hashesOfEntities.contains(key.hashCode()))
@@ -136,15 +217,23 @@ public class HashStorage<K, V> implements IHashStorage<K, V>
                 return null;
             }
             Entity foundEntity = findEntityByKey(key);
-            return foundEntity!=null ? foundEntity.getValue() : null;
+            return foundEntity != null ? foundEntity.getValue() : null;
         }
 
+        /**
+         * Represent a pair of key and value (K,V) as a single entity.
+         */
         @AllArgsConstructor
-        private class Entity
+        public class Entity
         {
             @Getter private K key;
             @Getter @Setter private V value;
 
+            @Override
+            public String toString()
+            {
+                return "[" + key + " : " + value + ']';
+            }
         }
     }
 }
